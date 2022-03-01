c7c08e096b7d14437cdc1f0535fd7f6f5c00c772
pavel.zeman <pavel.zeman@AHOJ-LOCHY-XD>
2022-03-01 09:01:34 +0100

    Přidejte soubory projektu.

---
+++ QRge/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using QRCoder;
+using QRCoder.Xaml;
+
+
+
+
+namespace QRge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string zadanyText = textboxVstup.Text;
+
+            QRCodeData dataQRkodu = QRCodeGenerator.GenerateQrCode(zadanyText, QRCodeGenerator.ECCLevel.M);
+
+            XamlQRCode qrKod = new XamlQRCode(dataQRkodu);
+
+            DrawingImage vkladanyObrazek = qrKod.GetGraphic(30);
+            obrazekVystup.Source = vkladanyObrazek;
+        }
+    }
+}
